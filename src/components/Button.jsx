d9f14289047ebd5015d2ad87d388a7cb62ec1eef
Ayran <ayranvieira29@gmail.com>
2024-12-12 22:03:37 -0300

    Deixando os botoes direcionaveis

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-export default function Button({ Icon, name, link}){
+export default function Button({ Icon, name, onClick}){
     
     return(
-    <div className="pt-2">
-        <button  className="bg-purple-600 font-mono font-bold text-white max-h-9 w-26 gap-2 items-center flex border-none rounded active:scale-95 hover:bg-purple-900 hover:translate-y-[2px] hover:text-purple-400 hover:duration-75">{Icon} {name} {link}</button>
+    <div  onClick={onClick} className="pt-2">
+        <button  className="bg-purple-600 font-mono font-bold text-white max-h-9 w-26 gap-2 items-center flex border-none rounded active:scale-95 hover:bg-purple-900 hover:translate-y-[2px] hover:text-purple-400 hover:duration-75">{Icon} {name} </button>
     </div>
     )
 }
--- src/components/CardsAbout.jsx
+++ src/components/CardsAbout.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import  Button  from "./Button";
+
 import { FaDownload } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import  Button  from "./Button";
 import { FaGithub } from "react-icons/fa";
 
 
 
 export default function CardsAbout (){
+    
+        const handleDownload = () => {
+          
+          const fileUrl = "../public/cv.pdf"
+          const link = document.createElement("a");
+          link.href = fileUrl;
+          link.download = "Curriculo_AyranDev.pdf";
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }
    
     return(
         <section className="min-h-screen bg-zinc-950">
@@ -14,9 +26,15 @@ export default function CardsAbout (){
                 <div className=" flex justify-center border-2 border-slate-300 h-[400px] w-[350px] shadow-2xl shadow-purple-400 rounded-xl hover:scale-110">
                     <div>
                         <h1 className=" pt-6 font-mono text-purple-600 text-3xl">Soft-Skills</h1>
+                        <span>
+                            <li>
+                                eu sou bom
+                            </li>
+                        </span>
                     </div>
+                    
                      <div className="self-end absolute pb-4 flex gap-6">
-                       <Button  name="LinkedIn" Icon={<FaLinkedin/>}/>
+                       <Button onClick={() => window.open("https://www.linkedin.com/in/ayran-vieira-dev/","_blank")} name="LinkedIn" Icon={<FaLinkedin/>}/>
                        <Button  name="Big Five" Icon={<FaDownload/>}/>
                     </div>
                 </div>
@@ -25,8 +43,8 @@ export default function CardsAbout (){
                         <h1 className=" pt-6 font-mono text-purple-600 text-3xl">Hard-Skills</h1>
                     </div>
                      <div className="self-end absolute pb-4 flex gap-6">
-                       <Button  name="GitHub" Icon={<FaGithub/>}/>
-                       <Button  name="Curriculo" Icon={<FaDownload/>} />
+                       <Button onClick={() => window.open("https://github.com/ayrandev","_blank")} name="GitHub" Icon={<FaGithub/>}/>
+                       <Button onClick={handleDownload} name="Currículo" Icon={<FaDownload/>} download="Curriculo_AyranDev.pdf"/>
                     </div>
                 </div>
             </div>
